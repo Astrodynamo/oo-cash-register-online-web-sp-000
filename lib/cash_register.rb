688fc81ceace0d7485f210b350aab3a85451cd7f
Amanda King <amapickell@gmail.com>
2019-07-11 23:43:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
-
+class CashRegister
+  attr_accessor 
+  
+  def initialize (discount = nil)
+    @total = 0
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item (title, price, quantity = 1)
+    @total += price
+  end
+  
+  def apply_discount
+    
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+    
+  end
+  
+end
